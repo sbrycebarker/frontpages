17e7766f640ce44ed290b1bf0febfeeb9d416252
sbrycebarker <sbrycesti@gmail.com>
2018-11-22 02:10:45 -0700

    drop

---
+++ index.js
@@ -0,0 +1,16 @@
+// let header = document.getElementById('header');
+// let dropdown = document.getElementById("header-drpdwn");
+//
+// header.addEventListener("mouseover", dropdownFunction)
+//
+// function dropdownFunction() {
+//   if (dropdown.style.height = "0px") {
+//     console.log("open")
+//     dropdown.style.height = '150px'
+//   } else {
+//     console.log("close")
+//     dropdown.style.height = "0px"
+//   }
+// }
+//
+// header.addEventListener("mouseout", dropdownFunction)
