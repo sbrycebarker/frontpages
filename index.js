c8a1bbdc765768c588a69c47e3a62f37ce0916e9
sbrycebarker <sbrycesti@gmail.com>
2018-11-22 16:06:09 -0700

    js works

--- index.js
+++ index.js
@@ -3,36 +3,31 @@ let dropdown = document.getElementById("header-drpdwn");
 let nav = document.getElementById("nav");
 let ecommerce = document.getElementById("ecomm-con");
 let home = document.getElementById('home');
-let dropitem = document.querySelectorAll("div.dropitem");
+let dropcon = document.getElementById("drop-con");
 
-// home.addEventListener("mouseover", dropDown);
-// home.addEventListener("mouseout", upfunction);
-header.addEventListener("mouseover", dropDown);
-header.addEventListener("mouseout", upfunction);
+nav.addEventListener("mouseover", dropDown);
+nav.addEventListener("mouseout", upfunction);
+home.addEventListener("mouseover", dropDown);
+home.addEventListener("mouseout", upfunction);
+
 
 function dropDown() {
-  console.log(home)
-  console.log(dropitem)
   dropdown.style.height = "200px"
-  setTimeout(function(){
-    dropitem[0].style.visibility = "visible";
-    dropitem[1].style.visibility = "visible";
-    dropitem[2].style.visibility = "visible";
-  }, 700)
 }
 function upfunction() {
-    dropitem[0].style.visibility = "hidden";
-    dropitem[1].style.visibility = "hidden";
-    dropitem[2].style.visibility = "hidden";
-    dropdown.style.height = "0"
+    dropdown.style.height = "0";
 }
 
 nav.addEventListener("mouseover", darkOut);
 nav.addEventListener("mouseout", lightback);
+home.addEventListener("mouseover", darkOut);
+home.addEventListener("mouseout", lightback);
 function darkOut() {
   ecommerce.style.opacity = .5;
+  dropcon.style.opacity = 1;
 }
 
 function lightback() {
   ecommerce.style.opacity = 1;
+  dropcon.style.opacity = 0;
 }
