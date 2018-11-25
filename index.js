8892b8023d33b640cbee310483e9d6c6b318f6fa
sbrycebarker <sbrycesti@gmail.com>
2018-11-24 17:44:51 -0700

    menus function

--- index.js
+++ index.js
@@ -4,34 +4,45 @@ let nav = document.getElementById("primary-nav");
 let ecommerce = document.getElementById("ecomm-con");
 let home = document.getElementById('home');
 let learn = document.getElementById("learn");
-let buy = document.getElementById("search");
 let learncon = document.getElementById("learn-con");
+let buy = document.getElementById("buy-con");
+let own = document.getElementById("own-con");
+let connect = document.getElementById('connect-con');
+let products = document.getElementById('products-con');
+let search = document.getElementById('search-con');
 // let dropcon = document.getElementById("drop-con");
+
+let menus = [learncon, buy, own, connect, products, search]
 
 nav.addEventListener("mouseover", dropDown);
 nav.addEventListener("mouseout", upfunction);
 dropdown.addEventListener("mouseout", upfunction);
 
-
-function dropDown() {
-dropdown.style.height = "400px";
-}
-function upfunction() {
-    dropdown.style.height = "0";
-}
-
-
-learn.addEventListener("mouseover", showMenu);
-buy.addEventListener("mouseover", hideMenu);
-
  function showMenu(menu) {
+   // dropDown()
+   for (i = 0; i < menus.length; i++) {
+     console.log("i", menus[i])
+     menus[i].style.opacity = '0';
+     menus[i].style.display = 'none';
+   }
+   menu = document.getElementById(menu);
    console.log("showMenu", menu)
-   learncon.style.opacity = 1;
+   menu.style.opacity = 1;
+   menu.style.display = 'flex';
+ }
+
+ function dropDown() {
+ dropdown.style.height = "400px";
+ }
+ function upfunction() {
+     dropdown.style.height = "0";
  }
 
  function hideMenu(menu){
-  console.log("hideMenu", menu)  
-  learncon.style.opacity = 0;
+   menu = document.getElementById(menu);
+  console.log("hideMenu", menu)
+   menu.style.opacity = 0;
+   menu.style.display = 'none';
  }
 
 nav.addEventListener("mouseover", darkOut);
