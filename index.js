f6d4312aebe707d278fbc9c70a633d3b9eed62f6
sbrycebarker <sbrycesti@gmail.com>
2018-11-25 00:50:14 -0700

    search

--- index.js
+++ index.js
@@ -10,37 +10,55 @@ let own = document.getElementById("own-con");
 let connect = document.getElementById('connect-con');
 let products = document.getElementById('products-con');
 let search = document.getElementById('search-con');
+let hide = document.getElementById('hide');
 // let dropcon = document.getElementById("drop-con");
 
 let menus = [learncon, buy, own, connect, products, search]
 
-nav.addEventListener("mouseover", dropDown);
-nav.addEventListener("mouseout", upfunction);
-dropdown.addEventListener("mouseout", upfunction);
+// nav.addEventListener("mouseover", dropDown);
+// nav.addEventListener("mouseout", upfunction);
+// dropdown.addEventListener("mouseout", upfunction);
 
  function showMenu(menu) {
-   // dropDown()
+   dropDown(menu);
    for (i = 0; i < menus.length; i++) {
-     console.log("i", menus[i])
+     // console.log("i", menus[i]);
      menus[i].style.opacity = '0';
      menus[i].style.display = 'none';
    }
    menu = document.getElementById(menu);
-   console.log("showMenu", menu)
    menu.style.opacity = 1;
    menu.style.display = 'flex';
  }
 
- function dropDown() {
- dropdown.style.height = "400px";
- }
+ function dropDown(menu) {
+   if (menu !== 'search-con') {
+     dropdown.style.height = "400px";
+   }
+   if (menu === 'search-con') {
+     dropdown.style.backgroundColor  = "#01FB01";
+     dropdown.style.height = "250px";
+   }
+
+   }
+
+function newFunction() {
+  for (i = 0; i < menus.length; i++) {
+    // console.log("i", menus[i]);
+    menus[i].style.opacity = '0';
+    menus[i].style.display = 'none';
+  }
+  dropdown.style.height = "200px"
+  dropdown.style.backgroundColor  = "#01FB01";
+}
+
  function upfunction() {
-     dropdown.style.height = "0";
+   dropdown.style.height = "0";
+   dropdown.style.backgroundColor = "#1a1818";
  }
 
  function hideMenu(menu){
    menu = document.getElementById(menu);
-  console.log("hideMenu", menu)
    menu.style.opacity = 0;
    menu.style.display = 'none';
  }
@@ -51,12 +69,24 @@ nav.addEventListener("mouseover", darkOut);
 nav.addEventListener("mouseout", lightback);
 function darkOut() {
   ecommerce.style.opacity = .5;
-    // setTimeout(function() {
-      // dropcon.style.opacity = 1;
-    // }, 500)
+
 }
 
 function lightback() {
   ecommerce.style.opacity = 1;
   // dropcon.style.opacity = 0;
 }
+
+window.addEventListener('scroll', function(evt) {
+    let pos = window.scrollY
+    if (pos !== 0) {
+    hide.style.display = "none"
+    header.style.height = "75px"
+    home.style.width = "75px"
+  } else {
+      hide.style.display = "flex";
+      home.style.width = "125px";
+      header.style.height = "125px";
+  }
+
+})
